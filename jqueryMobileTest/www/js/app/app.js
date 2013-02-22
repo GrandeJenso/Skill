b4b439cde4af60decb862b525cffee131923966e
Jens Lund <lund.jens@hotmail.com>
2013-02-22 16:27:03 +0100

    jQuery test
    testade listview, detaljerad vy, navbar, osv

--- jqueryMobileTest/www/js/app/app.js
+++ jqueryMobileTest/www/js/app/app.js
@@ -1,5 +1,8 @@
 var startApp = function() {
   // alert('started');
+    var jobID;
+    var xml_all;
+
 
 };
 
@@ -11,10 +14,12 @@ $(document).ready(function()
                          dataType: "xml",
                          success: parseXml
                          });
+
                   
                   function parseXml(xml)
                   {
                   
+                  xml_all=xml;
                   //find every Tutorial and print the author
                   $(xml).find("job").each(function()
                                                {
@@ -22,10 +27,11 @@ $(document).ready(function()
                                           var link = $(this).find('applicationMethods link').attr('href');
                                           var img = $(this).find('logo link').attr('href');
                                           var title = $(this).find('title').text();
+                                          var id = $(this).attr('id');
                                           
                                        
                                           
-                                          $('div#content ul#job').append('<li><a href="'+ link + '"><img src="' + img +'"><p>' + title + '</p></a></li>');
+                                          $('div#content ul#job').append('<li class="listelement" data-id= "' + id + '" ><a data-transition="slide"><img src="' + img +'"><p>' + title + '</p></a></li>');
                                           
                                           
                                          // $('div#content ul#job').append('<li><a href="'+$(this).find('applicationMethods link').attr('href') + '"><img src="' + $(this).find('logo link').attr('href') +'"><p>' + $(this).find('title').text() + '</p></a></li>');
@@ -37,7 +43,40 @@ $(document).ready(function()
                                           
                                                });
                   $('div#content ul#job').listview("refresh");
+                  
+//                  $('li.listelement').click(function() {
+//                                            var jobID = $(this).data('id');
+//                                            alert(jobID);
+//                                            
+//                                });
+                  
+                  $('li.listelement').live("click", function(){
+                                           jobID = $(this).data('id');
+                                           $.mobile.changePage("#two", {transition:"slide"} , true, true);
+                                           
+                                });
+                  
+                  
+                  
                   }
                   
+
+                  
                   });
+
+            $(document).on('pageinit', '#two', function()
+                           {
+                            
+                           var job = $(xml_all).find("job[id="+jobID+"]")
+                           var title = job.find('title').text();
+                           var detailed_div = $("div#detailed");
+                           var description = job.find('description').text();
+                           
+                           detailed_div.append("<h3>"+title+"</h3>");
+                           detailed_div.append("<p>"+description+"</p>");
+                           
+                           
+                           });
+
+
 
