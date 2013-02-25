dd5b5d2341d2a91e0ddd099e1fe482e5ce756400
Jens Lund <lund.jens@hotmail.com>
2013-02-25 10:25:20 +0100

    pageshow och loading
    bytte från pageinit till pageshow i app.js och lade till loading message

--- jqueryMobileTest/www/js/app/app.js
+++ jqueryMobileTest/www/js/app/app.js
@@ -2,12 +2,15 @@ var startApp = function() {
   // alert('started');
     var jobID;
     var xml_all;
+    var detailed_div;
 
 
 };
 
-$(document).ready(function()
+$(document).on('pageshow', '#one', function()
                   {
+               detailed_div = $("div#detailed");
+               detailed_div.text("");
                   $.ajax({
                          type: "GET",
                          url: "http://cv.skill.se/cv/rss.jsp?format=mtrxml&allads=1",
@@ -53,6 +56,7 @@ $(document).ready(function()
                   $('li.listelement').live("click", function(){
                                            jobID = $(this).data('id');
                                            $.mobile.changePage("#two", {transition:"slide"} , true, true);
+                                           $.mobile.loading( 'show' );
                                            
                                 });
                   
@@ -64,17 +68,15 @@ $(document).ready(function()
                   
                   });
 
-            $(document).on('pageinit', '#two', function()
+            $(document).on('pageshow', '#two', function()
                            {
-                            
                            var job = $(xml_all).find("job[id="+jobID+"]")
                            var title = job.find('title').text();
-                           var detailed_div = $("div#detailed");
                            var description = job.find('description').text();
                            
                            detailed_div.append("<h3>"+title+"</h3>");
                            detailed_div.append("<p>"+description+"</p>");
-                           
+                           $.mobile.loading( 'hide' );
                            
                            });
 
